d343cd1040b55a7a5f1f3696468189b4a0e3902b
Adam Laashiri <laashiri@gmail.com>
2023-01-23 12:05:27 +0100

    Added models

---
+++ PlacesDatabase/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlacesDB.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public float Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime Created { get; set; }
+
+        public virtual Post Post { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ PlacesDatabase/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PlacesDB.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int UserGroup { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime Created { get; set; }
+
+        // DateOfBirth
+        // Email
+
+        // Relations
+        public virtual ICollection<Post> Posts { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+
+        /*
+        public bool VerifyPassword(string input)
+        {
+            // Salt will always be the half of password column regardless of keysize
+            byte[] salt = Hasher.GenerateSalt(Password.Substring(Password.Length / 2, Password.Length / 2));
+            return Password.SequenceEqual(Hasher.HashPassword(input, salt));
+        }
+        */
+    }
+}
