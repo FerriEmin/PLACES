097804a65173259c2ea8a686e24ef9af2ae02131
Sophie <sophie.lindstrom90@gmail.com>
2023-01-26 13:57:47 +0100

    added global user and layout.

--- PlacesFrontEnd/MauiAppTEST/App.xaml.cs
+++ PlacesFrontEnd/MauiAppTEST/App.xaml.cs
@@ -1,10 +1,22 @@
 ﻿namespace MauiAppTEST;
 
+using MauiAppTEST.Services;
+using MauiAppTEST.TestData;
+
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
-		MainPage = new AppShell();
+
+        GlobalService.user = new()
+        {
+            UserName = "MonaLisa",
+            FirstName = "Mona",
+            LastName = "Lisa",
+            DateOfBirth = new DateTime(1980, 12, 11),
+        };
+
+        MainPage = new AppShell();
 	}
 }
