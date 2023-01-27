dd71a40628ab0ed5ad5ba65543d0ed9b5a460159
PontusHedman <ponhed@gmail.com>
2023-01-27 03:14:39 +0100

    Added map functionality

--- PlacesFrontEnd/MauiAppTEST/Services/LocationService.cs
+++ PlacesFrontEnd/MauiAppTEST/Services/LocationService.cs
@@ -7,29 +7,38 @@ using Microsoft.Maui.Devices.Sensors;
 
 namespace MauiAppTEST.Services
 {
+    //Add this to ANDROID
+    //Add the assembly-based permission:
+    //Open the Platforms/Android/MainApplication.cs file and add the following assembly attributes after using directives:
+
+    //[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+    //[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+    //[assembly: UsesFeature("android.hardware.location", Required = false)]
+    //[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+    //[assembly: UsesFeature("android.hardware.location.network", Required = false)]
     public class LocationService
     {
         //Get current location if possible, else get last known location.
         //Throw error 
         public async Task showLocationError()
         {
-            await Shell.Current.DisplayAlert("Location error", "Couldn't retrieve location data", "cancel");
+            await Shell.Current.DisplayAlert("Location error", "Couldn't retrieve location data", "Cancel");
         }
 
-        public async Task<(double?, double)> GetLocation()
+        public async Task<Location> GetLocation()
         {
             try
             {
                 Location location = await Geolocation.GetLocationAsync();
                 if (location != null)
                 {
-                    return (location.Altitude, location.Latitude);
+                    return location;
                 } else
                 {
                     location = await Geolocation.GetLastKnownLocationAsync();
                     if (location != null)
                     {
-                        return (location.Altitude, location.Latitude);
+                        return location;
                     }
                     else
                     {
--- PlacesFrontEnd/MauiAppTEST/ViewModel/MapViewModel.cs
+++ PlacesFrontEnd/MauiAppTEST/ViewModel/MapViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using MauiAppTEST.Services;
 using MauiAppTEST.View;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls.Maps;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +13,10 @@ namespace MauiAppTEST.ViewModel
 {
     public partial class MapViewModel : BaseViewModel
     {
+        
         public MapViewModel()
         {
-
+            
         }
 
     }
