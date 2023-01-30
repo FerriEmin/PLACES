7a8f28bfa39611f7d60f5aecdc6339949e00ab9d
Ferri_Emin <ferri_emin@hotmail.com>
2023-01-31 00:08:30 +0100

    CORS fix

--- PlacesBackEnd/Program.cs
+++ PlacesBackEnd/Program.cs
@@ -4,11 +4,21 @@ using PlacesDB;
 using PlacesDB.Models;
 using PlacesBackEnd.CRUD;
 
+const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors((options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+                      });
+}));
 
 var app = builder.Build();
 
@@ -18,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
