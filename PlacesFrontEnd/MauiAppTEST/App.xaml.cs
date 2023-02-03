6d005563cc2d373514642f9117e34495a5bd24a3
PontusHedman <ponhed@gmail.com>
2023-02-03 17:17:14 +0100

    Added custom Entrycomponent to remove underline

--- PlacesFrontEnd/MauiAppTEST/App.xaml.cs
+++ PlacesFrontEnd/MauiAppTEST/App.xaml.cs
@@ -2,6 +2,10 @@
 using MauiAppTEST.Services;
 using MauiAppTEST.Models;
 using MauiAppTEST.View;
+using MauiAppTEST.CustomUI;
+using Microsoft.Maui.Platform;
+using Microsoft.Maui.Handlers;
+
 
 namespace MauiAppTEST;
 
@@ -10,6 +14,26 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
+
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(IView.Background), (handler, view) =>
+        {
+            if (view is CustomEntry)
+            {
+                var color = view.Background.ToColor();
+                Color apa = Color.FromRgb(color.Red, color.Green, color.Blue);
+
+#if ANDROID
+                handler.PlatformView.SetBackgroundColor(apa.ToPlatform());
+#elif IS
+                handler.PlatformView.BackgroundColor = Colors.LightGray.ToPlatform();
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.Line;
+#elif WINDOWS
+                handler.PlatformView.Background = Colors.LightGray.ToPlatform();
+#endif
+
+            }
+        });
 
         GlobalService.user = new()
         {
