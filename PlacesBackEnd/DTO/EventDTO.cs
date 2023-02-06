cd40d663aef53c655de76197e839984f4759c3d0
Adam Laashiri <laashiri@gmail.com>
2023-02-06 10:39:13 +0100

    Updated eventDTO to reflect new database changes

--- PlacesBackEnd/DTO/EventDTO.cs
+++ PlacesBackEnd/DTO/EventDTO.cs
@@ -10,9 +10,28 @@ namespace PlacesBackEnd.DTO
         public string Description { get; set; }
         public string Image { get; set; }
         public int Likes { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+
+        public List<(string, string)> Comments { get; set; }
 
         public EventDTO () { }
         public EventDTO(Event @event) =>
-            (Title, Description, Image, Likes) = (@event.Title, @event.Description, @event.Image, @event.Reviews.Where(x => x.Like == true).Count());
+            (
+            Title,
+            Description,
+            Image,
+            Likes,
+            Comments
+            ) =
+            (
+            @event.Title,
+            @event.Description,
+            @event.Image,
+            @event.Reviews.Where(x => x.Like == true).Count(),
+            @event.Reviews.Select(x => (x.User.Username, x.Comment)).ToList()
+            );
     }
 }
